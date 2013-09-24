aa029f3a2b2822352139df3161f1932c79d8c49d
James Kyburz <james.kyburz@gmail.com>
2013-09-24 13:37:57 +0200

    allow other encoding options than escape and unescape

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ var exports = module.exports = function (doc) {
     if (doc.cookie === undefined) doc.cookie = '';
     
     var self = {};
-    self.get = function (key) {
+    self.get = function (key, opts) {
+        if (!opts) opts = {};
+        var decode = opts.decode || unescape;
         var splat = doc.cookie.split(/;\s*/);
         for (var i = 0; i < splat.length; i++) {
             var ps = splat[i].split('=');
+            var k = decode(ps.shift());
+            if (k === key) return decode(ps.join('='));
         }
         return undefined;
     };
     
     self.set = function (key, value, opts) {
         if (!opts) opts = {};
-        var s = escape(key) + '=' + escape(value);
+        var encode = opts.encode || escape;
+        var s = encode(key) + '=' + encode(value);
         if (opts.expires) s += '; expires=' + opts.expires;
-        if (opts.path) s += '; path=' + escape(opts.path);
+        if (opts.path) s += '; path=' + encode(opts.path);
         doc.cookie = s;
         return s;
     };
